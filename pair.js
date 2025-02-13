23f1d31fa25f132041bb2eb4fa31ac4e733b528d
Casper tech <146692830+Traby-qriz@users.noreply.github.com>
2025-02-13 18:52:09 +0300

    POWERED BY 𝖢𝖠𝖲𝖯𝖤𝖱 𝖳𝖤𝖢𝖧

--- pair.js
+++ pair.js
@@ -85,7 +85,7 @@ router.get("/", async (req, res) => {
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://i.ibb.co/G9xgd0h/IMG-20250126-042719.png",
               },
               caption: sid,
             });
